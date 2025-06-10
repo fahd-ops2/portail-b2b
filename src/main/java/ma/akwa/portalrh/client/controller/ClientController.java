1eb67a32849185fbc332168b4b7895804cc72b43
Hiba Gargar <bhousni44@gmail.com>
2025-06-10 16:49:03 +0100

    AB

--- src/main/java/ma/akwa/portalrh/client/controller/ClientController.java
+++ src/main/java/ma/akwa/portalrh/client/controller/ClientController.java
@@ -29,7 +29,7 @@ public class ClientController {
     @ApiResponse(responseCode = "200", description = "Client créé avec succès")
     @PostMapping
     public ResponseEntity<ClientResponse> create(
-            @Parameter(description = "Données du produit à créer", required = true)
+            @Parameter(description = "Données du client à créer", required = true)
             @RequestBody ClientRequest request) {
         return ResponseEntity.ok(clientService.create(request));
     }
@@ -48,14 +48,14 @@ public class ClientController {
         return ResponseEntity.ok(clientService.update(id, request));
     }
 
-    @Operation(summary = "Supprimer un produit")
+    @Operation(summary = "Supprimer un client")
     @ApiResponses({
-            @ApiResponse(responseCode = "204", description = "Produit supprimé avec succès"),
-            @ApiResponse(responseCode = "404", description = "Produit non trouvé")
+            @ApiResponse(responseCode = "204", description = "Client supprimé avec succès"),
+            @ApiResponse(responseCode = "404", description = "Client non trouvé")
     })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(
-            @Parameter(description = "ID du produit à supprimer", required = true)
+            @Parameter(description = "ID du client à supprimer", required = true)
             @PathVariable Long id) {
         clientService.delete(id);
         return ResponseEntity.noContent().build();
@@ -63,22 +63,22 @@ public class ClientController {
 
 
 
-    @Operation(summary = "Récupérer un produit par son ID")
+    @Operation(summary = "Récupérer un client par son ID")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Produit récupéré"),
-            @ApiResponse(responseCode = "404", description = "Produit non trouvé")
+            @ApiResponse(responseCode = "200", description = "Client récupéré"),
+            @ApiResponse(responseCode = "404", description = "Client non trouvé")
     })
     @GetMapping("/{id}")
     public ResponseEntity<ClientResponse> getById(
-            @Parameter(description = "ID du produit à récupérer", required = true)
+            @Parameter(description = "ID du client à récupérer", required = true)
             @PathVariable Long id) {
         return ResponseEntity.ok(clientService.getById(id));
     }
 
 
 
-    @Operation(summary = "Récupérer une page de produits")
-    @ApiResponse(responseCode = "200", description = "Liste paginée des produits récupérée avec succès")
+    @Operation(summary = "Récupérer une page de clients")
+    @ApiResponse(responseCode = "200", description = "Liste paginée des clientts récupérée avec succès")
     @GetMapping
     public ResponseEntity<Page<ClientResponse>> getAllPaginated(
             @Parameter(description = "Taille de la page", example = "10", required = true)
@@ -86,7 +86,7 @@ public class ClientController {
             @Parameter(description = "Numéro de la page (0 = première page)", example = "0", required = true)
             @RequestParam int page) {
 
-        Pageable pageable = PageRequest.of(size, page);
+        Pageable pageable = PageRequest.of(page, size);
         return ResponseEntity.ok(clientService.getAllPaginated(pageable));
     }
 
